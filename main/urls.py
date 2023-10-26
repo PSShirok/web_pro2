c10f01f6429b2988a83208b542baa4a8ea2d217c
Павел <claid07@mail.ru>
2023-10-26 08:57:16 +0400

    add contacts/ in urls.path

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from main.views import contacts_index, home_index
 
 urlpatterns = [
-    path('', contacts_index),
+    path('contacts/', contacts_index),
     path('', home_index)
 ]
